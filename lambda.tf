42d70cabcd64bdb2ae23b0cbf8457cb7c4f11644
Julian Vega <ja.vega.g10@gmail.com>
2022-11-11 22:24:02 -0500

    Modify lambda.tf
    Add functions handlers to fix terraform plan issue

--- lambda.tf
+++ lambda.tf
@@ -3,6 +3,7 @@ resource "aws_lambda_function" "write_lambda" { #Create - Update - Delete
   filename      = "foo.zip"
   function_name = "http-write-${var.region}-${var.workspace}-${random_uuid.name.result}"
   role          = aws_iam_role.lambda_iam_write_dynamodb.arn
+  handler       = "index.hanlder"
 
   # The filebase64sha256() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the base64sha256() function and the file() function:
@@ -25,6 +26,7 @@ resource "aws_lambda_function" "read_lambda" {
   filename      = "foo.zip"
   function_name = "http-read-${var.region}-${var.workspace}-${random_uuid.name.result}"
   role          = aws_iam_role.lambda_iam_read_dynamodb.arn
+  handler       = "index.hanlder"
 
   source_code_hash = filebase64sha256("foo.zip")
 
