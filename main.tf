cb07e1570b7c83459b576746c069fe683f2e6987
Julian Vega <ja.vega.g10@gmail.com>
2022-11-11 19:11:12 -0500

    Infrastructure repository initialized

---
+++ main.tf
@@ -0,0 +1,25 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+  required_version = ">= 1.2.0"
+
+  cloud {
+    organization = "applaudo-devops"
+
+    workspaces {
+      name = "aws-lambdas-api"
+    }
+  }
+
+}
+
+provider "aws" {
+  region = var.region
+}
+
+resource "random_uuid" "name" {
+}
