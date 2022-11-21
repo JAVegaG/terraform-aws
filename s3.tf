7faf7748c52bcd9ef18e238c729a274cbd5e78c1
Julian Vega <ja.vega.g10@gmail.com>
2022-11-21 09:49:12 -0500

    Add public access policies on s3

--- s3.tf
+++ s3.tf
@@ -9,13 +9,22 @@ resource "aws_s3_bucket_versioning" "s3_versioning" {
   }
 }
 
-# resource "aws_s3_bucket_website_configuration" "s3-website-settings" {
-#   bucket = aws_s3_bucket.s3_bucket_website.bucket
+resource "aws_s3_bucket_ownership_controls" "example" {
+  bucket = aws_s3_bucket.s3_bucket_website.id
 
-#   index_document {
-#     suffix = "index.html"
-#   }
-# }
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "example" {
+  bucket = aws_s3_bucket.s3_bucket_website.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
 
 resource "aws_s3_bucket_policy" "allow_access_from_cloudfront" {
   bucket = aws_s3_bucket.s3_bucket_website.id
